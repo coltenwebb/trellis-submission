3ecffd6c694d85a54ad22382b26ed97adced39fd
Colten Webb <coltenwebb@gmail.com>
2021-01-02 17:32:52 -0600

    first commit

--- README.md
+++ README.md
@@ -1,46 +1,17 @@
-# Getting Started with Create React App
+# UI Project Submission for Trellis Internship
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+All the code I wrote for this mockup is in `src/App.{tsx,css}`.
 
-## Available Scripts
+This UI assumes that each field and gateway is 1-1 for grouping them together.
+None of that data is real, the mapped locations are screenshots from 
+Google Maps. There is an object called `sampleGatewayDatas` that contains
+all the data the UI displays.
 
-In the project directory, you can run:
+## Screenshots
 
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Overview of the dashboard
+![Screenshot1](screenshots/screenshot1.png)
+Support for warnings/errors
+![Screenshot2](screenshots/screenshot2.png)
+Expanded view
+![Screenshot3](screenshots/screenshot3.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,214 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+
+type GatewayData = {
+  name: string;
+  sensorDatas: SensorData[];
+}
+
+type SensorError = {
+  level: "error" | "warning";
+  msg: string;
+}
+
+type SensorData = {
+  name: string;
+  lastMessage: SensorMessage;
+  moisture: number[][];
+  temp: number[][];
+  errors: SensorError[];
+  lastReceived: number;
+}
+
+type SensorMessage = {
+  time_received: number;
+}
+
+type SensorCardProps = {
+  data: SensorData;
+}
+
+function SensorCard({data}: SensorCardProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  if (isExpanded) {
+    return (<ExpandedCard data={data} toggleExpanded={() => setIsExpanded(false)}/>);
+  } else {
+    return (<CollapsedCard data={data} toggleExpanded={() => setIsExpanded(true)}/>);
+  }
+}
+
+type CollapsedCardProps = {
+  data: SensorData;
+  toggleExpanded: Function;
+}
+
+function CollapsedCard({data, toggleExpanded}: CollapsedCardProps) {
+  return (
+    <div className="sensor-card" onClick={toggleExpanded}>
+      <img alt="location" src={data.locationImage} height="200" />
+      <div className="info">
+        <div className="card-title">
+        <h1>{data.name}</h1>
+        {
+          data.errors.map((err) => {
+            if (err.level === "warning") {
+              return <span key={err.msg} className="tooltip warning">&#9888;<span className="tooltiptext">{err.msg}</span></span>
+            } else {
+              return <span key={err.msg} className="tooltip error">&#9888;<span className="tooltiptext">{err.msg}</span></span>
+            }
+          })
+        }</div>
+        <p>Last Update: {moment(data.lastReceived).fromNow()}</p>
+        <p className="hint">Click to expand</p>
+      </div>
+    </div>
+  );
+}
+
+type ExpandedCardProps = {
+  data: SensorData;
+  toggleExpanded: Function;
+}
+
+function ExpandedCard({data, toggleExpanded}: ExpandedCardProps) {
+  return (
+    <div className="sensor-card expanded" onClick={toggleExpanded}>
+      <img alt="location" src={data.locationImage} height="200" />
+      <div className="info">
+        <div className="card-title">
+        <h1>{data.name}</h1>
+        {
+          data.errors.map((err) => {
+            if (err.level === "warning") {
+              return <span key={err.msg} className="tooltip warning">&#9888;<span className="tooltiptext">{err.msg}</span></span>
+            } else {
+              return <span key={err.msg} className="tooltip error">&#9888;<span className="tooltiptext">{err.msg}</span></span>
+            }
+          })
+        }</div>
+        <p>Last Update: {moment(data.lastReceived).fromNow()}</p>
+        <h2>Temperature</h2>
+        <div className="datavis">
+          <Line data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Depth 1',
+                borderColor: 'rgb(255, 99, 132)',
+                data: data.temp[0]
+            }, 
+            {
+                label: 'Depth 2',
+                borderColor: 'rgb(210, 99, 132)',
+                data: data.temp[1]
+            },
+            {
+                label: 'Depth 3',
+                borderColor: 'rgb(190, 99, 132)',
+                data: data.temp[2]
+            }]
+          }} options={{aspectRatio: 4}} width={null} height={null}/>
+        </div>
+        <h2>Moisture</h2>
+        <div className="datavis">
+          <Line data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Depth 1',
+                borderColor: 'rgb(99, 255, 132)',
+                data: data.moisture[0]
+            }, 
+            {
+                label: 'Depth 2',
+                borderColor: 'rgb(99, 230, 132)',
+                data: data.moisture[1]
+            },
+            {
+                label: 'Depth 3',
+                borderColor: 'rgb(99, 210, 132)',
+                data: data.moisture[2]
+            }]
+          }} options={{aspectRatio: 4}} width={null} height={null}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type GatewayProps = {
+  data: GatewayData;
+}
+
+function Gateway({data}: GatewayProps) {
+  return ( 
+      <div className="gateway">
+        <h2 className="gateway-title">{data.name}</h2>
+        <div className="card-box">
+          {data.sensorDatas.map((sensorData) => {
+            return <SensorCard key={sensorData.name} data={sensorData} />
+          })}
+        </div>
+      </div>
+  );
+}
+
+const sampleGatewayDatas: GatewayData[] = [{
+  name: "West Field",
+  sensorDatas: [
+    {
+      name: "Sensor 1",
+      locationImage: "location1.png",
+      errors: [],
+      moisture: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      temp: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      lastReceived: 1609629848432
+    },
+    {
+      name: "Sensor 2",
+      locationImage: "location2.png",
+      errors: [],
+      moisture: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      temp: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      lastReceived: 1609629848432
+    }
+  ]
+}, 
+{
+  name: "East Field",
+  sensorDatas: [
+    {
+      name: "Sensor 1",
+      locationImage: "location3.png",
+      errors: [{
+        level: "warning",
+        msg: "No data was received between 2am and 6am today."
+      }],
+      moisture: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      temp: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      lastReceived: 1609629848432
+    },
+    {
+      name: "Sensor 2",
+      locationImage: "location4.png",
+      errors: [],
+      moisture: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      temp: [[23,24,32,23,24,32,23],[18,22,25,18,22,25,18],[14,19,20,14,19,20,14]],
+      lastReceived: 1609629848432
+    }
+  ]
+}]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trellis Dashboard</h1>
       </header>
+      {sampleGatewayDatas.map((gatewayData) => 
+        <Gateway key={gatewayData.name} data={gatewayData}/>)}
+      
     </div>
   );
 }
